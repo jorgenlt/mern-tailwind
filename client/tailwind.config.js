38ec2424da74a74f4d1856e34a2e4b0026dd5702
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2024-02-21 10:46:28 +0800

    config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    // screens: {},
+    // colors: {
+      // transparent: 'transparent',
+      // black: '#000',
+      // white: '#fff',
+      // gray: {
+      //   100: '#f7fafc',
+      //   // ...
+      //   900: '#1a202c',
+      // },
+    // },
+    // fontFamily: {},
+    // spacing: {},
+    extend: {
+      // spacing: {},
+      // borderRadius: {},
+    },
   },
   plugins: [],
 };
