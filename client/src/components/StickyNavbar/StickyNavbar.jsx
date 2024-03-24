2d74ffecca33596e51d342625e71697d36d1d937
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2024-03-24 15:17:03 +0800

    update navbar

--- client/src/components/StickyNavbar/StickyNavbar.jsx
+++ client/src/components/StickyNavbar/StickyNavbar.jsx
@@ -4,21 +4,27 @@ import {
   Typography,
   Button,
   IconButton,
-  Card,
 } from "@material-tailwind/react";
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { setLogout } from "../../pages/Login/authSlice";
 
 const StickyNavbar = () => {
-  const [openNav, setOpenNav] = useState(false);
+  const [isNavOpen, setIsNavOpen] = useState(false);
 
   const userId = useSelector((state) => state.auth.user._id);
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
+  const handleNavigation = (destination) => {
+    if (isNavOpen) {
+      setIsNavOpen(false);
+    }
+    navigate(destination);
+  };
 
   const logout = async () => {
     try {
@@ -34,8 +40,6 @@ const StickyNavbar = () => {
         console.log(response.data.msg);
         dispatch(setLogout());
         navigate("/login");
-      } else {
-        console.error("Registration error:", response.data.error);
       }
     } catch (err) {
       console.error(err);
@@ -45,7 +49,7 @@ const StickyNavbar = () => {
   useEffect(() => {
     window.addEventListener(
       "resize",
-      () => window.innerWidth >= 960 && setOpenNav(false)
+      () => window.innerWidth >= 960 && setIsNavOpen(false)
     );
   }, []);
 
@@ -55,45 +59,28 @@ const StickyNavbar = () => {
         as="li"
         variant="small"
         color="blue-gray"
-        className="p-1 font-normal transition-colors hover:text-blue-500 focus:text-blue-500"
+        className="p-1 font-normal transition-colors hover:text-blue-500 focus:text-blue-500 cursor-pointer"
+        onClick={() => handleNavigation("/home")}
       >
-        <a href="#" className="flex items-center">
-          Pages
-        </a>
+        Home
       </Typography>
       <Typography
         as="li"
         variant="small"
         color="blue-gray"
-        className="p-1 font-normal transition-colors hover:text-blue-500 focus:text-blue-500"
+        className="p-1 font-normal transition-colors hover:text-blue-500 focus:text-blue-500 cursor-pointer"
+        onClick={() => handleNavigation(`/users/${userId}`)}
       >
-        <Link to={`users/${userId}`} className="flex items-center">
-          Account
-        </Link>
-
-        {/* <a href="#" className="flex items-center">
-          Account
-        </a> */}
+        Account
       </Typography>
       <Typography
         as="li"
         variant="small"
         color="blue-gray"
-        className="p-1 font-normal transition-colors hover:text-blue-500 focus:text-blue-500"
+        className="p-1 font-normal transition-colors hover:text-blue-500 focus:text-blue-500 cursor-pointer"
+        onClick={() => handleNavigation("/home")}
       >
-        <a href="#" className="flex items-center">
-          Blocks
-        </a>
-      </Typography>
-      <Typography
-        as="li"
-        variant="small"
-        color="blue-gray"
-        className="p-1 font-normal transition-colors hover:text-blue-500 focus:text-blue-500"
-      >
-        <a href="#" className="flex items-center">
-          Docs
-        </a>
+        Docs
       </Typography>
     </ul>
   );
@@ -101,8 +88,11 @@ const StickyNavbar = () => {
   return (
     <Navbar className="fixed top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4">
       <div className="flex items-center justify-between text-blue-gray-900">
-        <Typography className="mr-4 cursor-pointer py-1.5 font-medium transition-colors hover:text-blue-500 focus:text-blue-500">
-          <Link to="home">mern-tailwind</Link>
+        <Typography
+          className="mr-4 cursor-pointer py-1.5 font-medium transition-colors hover:text-blue-500 focus:text-blue-500"
+          onClick={() => handleNavigation("/home")}
+        >
+          mern-tailwind
         </Typography>
         <div className="flex items-center gap-4">
           <div className="mr-4 hidden lg:block">{navList}</div>
@@ -113,16 +103,16 @@ const StickyNavbar = () => {
               className="hidden lg:inline-block"
               onClick={logout}
             >
-              <span>Sign out</span>
+              Sign out
             </Button>
           </div>
           <IconButton
             variant="text"
             className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
             ripple={false}
-            onClick={() => setOpenNav(!openNav)}
+            onClick={() => setIsNavOpen(!isNavOpen)}
           >
-            {openNav ? (
+            {isNavOpen ? (
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -155,14 +145,11 @@ const StickyNavbar = () => {
           </IconButton>
         </div>
       </div>
-      <Collapse open={openNav}>
+      <Collapse open={isNavOpen}>
         {navList}
-        <div className="flex items-center gap-x-1">
-          <Button fullWidth variant="text" size="sm" className="">
-            <span>Log In</span>
-          </Button>
-          <Button fullWidth variant="gradient" size="sm" className="">
-            <span>Sign in</span>
+        <div className="flex gap-x-1">
+          <Button variant="text" size="sm" onClick={logout}>
+            Sign out
           </Button>
         </div>
       </Collapse>
